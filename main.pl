cdf2a84f6bbd20894855d7d25874b10d4388517b
Lucas do Vale <lukasvale22@gmail.com>
2018-05-15 22:37:21 -0300

    comentários adicionados e pequenas modificações

--- main.pl
+++ main.pl
@@ -2,14 +2,15 @@ use warnings;
 use strict;
 
 my $nomeArqReg = "registro.txt";
-my $inicioArq = 1;
+#Constantes padrão que definem início e fim de um arquivo
+my $inicioArq = 1;  
 my $fimArq = 2;
 
-sub resgistraOpcao {
+#Armazena a primeira linha do arquivo de registro (que contém a opção) a ser passada pelo gerenciador C++
+sub registraOpcao {
 
 	open(my $entrada, "<:encoding(UTF-8)", $_[0]) or die "Erro! O arquivo não pode ser aberto: $!";
 
-	seek($entrada,0,$inicioArq);
 	my $opcao = <$entrada>;
 	chomp($opcao);
 	
@@ -18,22 +19,24 @@ sub resgistraOpcao {
 	return $opcao;
 }
 
+#Armazena a string de interesse da segunda linha do arquivo de registro (para análise de expressão regular) passada pelo gerenciador C++
 sub registraString{
 
 	my $strDsj;
 
 	open(my $entrada, "<:encoding(UTF-8)", $_[0]) or die "Erro! O arquivo não pode ser aberto: $!";
 
-	while(<$entrada>){
-			$strDsj = <$entrada>;
-			chomp($strDsj);
-			seek($entrada,0,$fimArq);
+	if(<$entrada>){
+		$strDsj = <$entrada>;
+		chomp($strDsj);
 	}
+
 	close $entrada or die "$entrada: $!";
 
 	return $strDsj;
 }
 
+#filtra nomes de arquivo por string passada no arquivo de registros
 sub filtraString{
 
 	(my $nomeArq, my $strDsj) = @_;
@@ -42,15 +45,17 @@ sub filtraString{
 	my @lista;
 
 	open(my $entrada, "<:encoding(UTF-8)", $nomeArq) or die "Erro! O arquivo não pode ser aberto: $!";
-	seek($entrada,0,$inicioArq);
 
+	#percorre cada linha do arquivo
 	while (<$entrada>) {
 
 		chomp($_);
-		my @sptLinha = split(/::/, $_);
+		my @sptLinha = split(/::/, $_); #separa identificador e nome
 
+		#toda vez que encontrar identificador = N (nome)
 		if($sptLinha [0] eq 'N'){
 
+			#verifica se a string desejada está contida no nome do arquivo e armazena o mesmo numa lista
 			if($sptLinha [1] =~ /$strDsj/){
 				$ocorrencia++;
 				$lista [$ocorrencia - 1] = $sptLinha [1];
@@ -66,50 +71,50 @@ sub filtraString{
 
 }
 
-sub geraArqLista {
-
-	unlink "lista-de-retorno.txt";
-	open(my $entrada, ">>:encoding(UTF-8)", "lista-de-retorno.txt") or die "Erro! O arquivo não pode ser gerado: $!";
-	seek($entrada,0,$inicioArq);
-	foreach (@_) {
-		print $entrada "$_\n";
-		print "$_\n";
-	}
-	close $entrada or die "$entrada: $!";
-
-}
-
+#conta ocorrencias de uma string desejada dentro de cada arquivo
 sub contaOcorrencias {
 
 	(my $nomeArq, my $strDsj) = @_;
 
 	my @lista;
 	my @entrada;
-	my $numLinhasArqReg = 0;
+	my $numLinhasArqReg = 0; #numero de linhas de cada arquivo 
+	my $numLinOcorr = 0;	#numero de linhas em que aparecem a string
+	my $ocorrenciaTotal = 0;	#ocorrencia total em todos os arquivos
+
+	$lista [$numLinhasArqReg] = "Ocorrencia\tLinhas\t\tNome";
+	$numLinhasArqReg++;
 
 	open(my $entradaReg, "<:encoding(UTF-8)", $nomeArq) or die "Erro! O arquivo não pode ser aberto: $!";
 
+	#percorre as linhas do arquivo de registro
 	while (<$entradaReg>) {
 
 		chomp($_);
 		my @sptLinha = split(/::/, $_);
 
+		#quando encontrar um nome de arquivo abre esse arquivo
 		if($sptLinha [0] eq 'N'){
 			
-			my $ocorrencia = 0;
+			my $ocorrenciaArq = 0; #ocorrencia em cada arquivo
 			
 			open(my $entradaArq, "<:encoding(UTF-8)", $sptLinha [1]) or die "Erro! O arquivo não pode ser aberto: $!";
 
+			#percorre todas as linhas do arquivo aberto e conta em quantas linhas há ocorrencia e quantas ocorrencias naquele arquivo
 			while (<$entradaArq>) {
-
+				if ($_ =~ /$strDsj/) {
+					$numLinOcorr++; 
+				}
 				while ($_ =~ m/($strDsj)/g){
-					$ocorrencia++;
+					$ocorrenciaArq++;
 				}
 			}
+			$ocorrenciaTotal += $ocorrenciaArq;
 
 			close $entradaArq or die "$entradaArq: $!";
 			
-			$lista [$numLinhasArqReg] = "$sptLinha[1]: $ocorrencia";
+			#salva numa lista todas ocorencias de cada arquivo e em quantas linhas se deram
+			$lista [$numLinhasArqReg] = "$ocorrenciaArq\t\t$numLinOcorr\t\t$sptLinha[1]";
 			$numLinhasArqReg++;
 
 		}
@@ -118,20 +123,44 @@ sub contaOcorrencias {
 
 	close $entradaReg or die "$entradaReg: $!";
 
-	return @lista;
+	return $ocorrenciaTotal, @lista;
 
 }
 
-my $opcao = resgistraOpcao($nomeArqReg);
+#gera arquivo contendo uma lista com as saídas desejadas (filtragem) de acordo com cada opção passada no arquivo de registro
+sub geraArqLista {
+
+	unlink "lista-de-retorno.txt";
+	open(my $entrada, ">>:encoding(UTF-8)", "lista-de-retorno.txt") or die "Erro! O arquivo não pode ser gerado: $!";
+	
+	foreach (@_) {
+		print $entrada "$_\n";
+		print "$_\n";
+	}
+	close $entrada or die "$entrada: $!";
+
+}
+
+#programa principal que extrai dados importantes do arquivo de registros
+
+system("cls");
+print ("*************************************\n");
+print ("********PROCESSADOR DE TEXTOS********\n");
+print ("*************************************\n\n");
+
+
+my $opcao = registraOpcao($nomeArqReg);
 my $stringDesejada = registraString($nomeArqReg);
 my @lista;
+my $ocorrencia;
 
 if ($opcao eq "FNEL"){
-	my $ocorrencia;
+	
+	print("<Filtrando por nome de arquivo...>\n");
+	print ("String desejada: $stringDesejada\n\n");
 	($ocorrencia, @lista) = filtraString($nomeArqReg, $stringDesejada);
-	print("Filtrar por string: $stringDesejada\n");
+	
 	print("Numero de ocorrencias encontradas: ", $ocorrencia);
-
 	print("\nArquivos encontrados: \n"); 
 	if($ocorrencia == 0){
 		print("NENHUM!")
@@ -142,7 +171,11 @@ if ($opcao eq "FNEL"){
 
 elsif ($opcao eq "CONT"){
 
-	@lista = contaOcorrencias ($nomeArqReg, $stringDesejada);
+	print("<Filtrando por conteúdo de arquivo...>\n");
+	print ("String desejada: $stringDesejada\n\n");
+	($ocorrencia, @lista) = contaOcorrencias ($nomeArqReg, $stringDesejada);
+
+	print("Numero de ocorrencias totais encontradas: ", $ocorrencia, "\n\n");
 	geraArqLista (@lista)
 }
 
