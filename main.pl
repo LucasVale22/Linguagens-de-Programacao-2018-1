d97aad2fa6c707c394412564d2bf7901c7ce843c
Lucas do Vale <lukasvale22@gmail.com>
2018-05-25 00:58:25 -0300

    programa mais organizado

--- main.pl
+++ main.pl
@@ -1,13 +1,6 @@
 use warnings;
 use strict;
-use fileManager qw (registraParametros filtraString contaOcorrencias converteDataHora filtraDataHora atualizaRegistro geraArqLista);
-
-my $tabela = Text::TabularDisplay->new;
-
-#Constantes padrão que definem início e fim de um arquivo
-my $inicioArq = 0;
-my $atualArq = 1; 
-my $fimArq = 2;
+use fileManager qw (registraParametros filtraString contaOcorrencias converteDataHora filtraDataHora atualizaRegistro filtraTamanho geraArqLista);
 
 #programa principal que extrai dados importantes do arquivo de registros
 
@@ -18,62 +11,54 @@ print ("*************************************\n\n");
 
 my @lista;
 my $ocorrencia;
-my $nomeArqReg = "registroTemp.txt";
-(my $opcao, my $stringDesejada) = registraParametros($nomeArqReg);
+my $arqParametros = "parametros.txt";
+(my $opcao, my $stringDesejada, my $arqRegistro) = registraParametros($arqParametros);
 
 if ($opcao eq "FSTR"){
 	
 	print("<Filtrando por nome de arquivo...>\n");
 	print ("String desejada: $stringDesejada\n\n");
-	($ocorrencia, @lista) = filtraString($nomeArqReg, $stringDesejada);
+	($ocorrencia, @lista) = filtraString($arqRegistro, $stringDesejada);
 	
 	print("Numero de ocorrencias: ", $ocorrencia);
 
-	if ($ocorrencia == 0) {
-		print ("\n\n*** NENHUM RESULTADO ENCONTRADO! ***\n");
-	}
-	else {
-		print("\nArquivos encontrados: \n"); 
-		for my $href ( @lista ) {
-	    	$tabela->add($href->{nome}, $href->{diretorio}, $href->{dataHora}, $href->{tamanho});
-		}
-
-		geraArqLista ($tabela);
-	}
+	geraArqLista ($ocorrencia, @lista);
 }
 
-elsif ($opcao eq "CONT"){
+elsif ($opcao eq "FCON"){
 
 	print("<Filtrando por conteúdo de arquivo...>\n");
 	print ("String desejada: $stringDesejada\n\n");
-	($ocorrencia, @lista) = contaOcorrencias ($nomeArqReg, $stringDesejada);
+	($ocorrencia, @lista) = contaOcorrencias ($arqRegistro, $stringDesejada);
 
 	print("Numero de ocorrencias totais encontradas: ", $ocorrencia, "\n\n");
 
-	for my $href ( @lista ) {
-    	$tabela->add($href->{nome}, $href->{ocorrenciaArq}, $href->{numLinOcorr});
-	}
-
-	geraArqLista ($tabela);
+	geraArqLista ($ocorrencia, @lista);
+	
 }
 
 elsif ($opcao eq "FDAT"){
 
 	print("<Filtrando por periodo de modificacao de arquivo...>\n\n");
 	print ("Periodo de Data desejada: $stringDesejada\n\n");
-	($ocorrencia, @lista) = filtraDataHora ($nomeArqReg, $stringDesejada);
+	($ocorrencia, @lista) = filtraDataHora ($arqRegistro, $stringDesejada);
 
-	for my $href ( @lista ) {
-    	$tabela->add($href->{nome}, $href->{diretorio}, $href->{dataHora}, $href->{tamanho});
-	}
+	geraArqLista ($ocorrencia, @lista);
+}
 
-	geraArqLista ($tabela);
+elsif ($opcao eq "FTAM"){
+
+	print("<Filtrando por faixa de tamanho de arquivo...>\n\n");
+	print ("Faixa desejada: $stringDesejada\n\n");
+	($ocorrencia, @lista) = filtraTamanho ($arqRegistro, $stringDesejada);
+
+	geraArqLista ($ocorrencia, @lista);
 }
 
 elsif ($opcao eq "MREG"){
 
 	print("<Atualizando Arquivo de Registro...>\n\n");
-	atualizaRegistro($nomeArqReg);
+	atualizaRegistro($arqRegistro);
 	print("\nArquivo de Registro Atualizado com sucesso\n");
 }
 
